592cfec1b4029ee2a768747bce8969996d8e4078
Brian Teng <47837288+BETeng@users.noreply.github.com>
2019-08-17 12:09:16 -0500

    Update Posts.js

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -15,7 +15,7 @@ class Posts extends Component {
     loadPosts = page => {
         list(page).then(data => {
             //if (data.error) {
-                console.log("LOL");
+               // console.log("LOL");
            // } else {
                 this.setState({ posts: data });
            // }
